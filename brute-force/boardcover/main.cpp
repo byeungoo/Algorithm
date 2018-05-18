60e68d25645ebb32963f3865c055bd025fc89140
hoon <dksekfldks65@naver.com>
2018-05-18 13:56:29 +0900

    gameboard 문제 완료

--- brute-force/boardcover/main.cpp
+++ brute-force/boardcover/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void solve(char gameBoard[][21], int curPoint, int x, int y);
+void solve(char gameBoard[][25], int puzzleNum);
 int pointCnt = 0;
 int h = 0, w = 0;
 int result = 0;
+int boardPuzzle = 0;
 
 int main(void)
 {
@@ -15,7 +17,7 @@ int main(void)
 
 		pointCnt = 0;
 		cin >> h >> w;
-		char gameBoard[21][21] = { '#', };
+		char gameBoard[25][25] = { '#', };
 		result = 0;
 		//board판 초기화
 		for (int i = 1; i <= h; i++) {
@@ -25,89 +27,85 @@ int main(void)
 					pointCnt++;
 			}
 		}
+		boardPuzzle = pointCnt / 3;
 
 		if (pointCnt % 3 != 0) { //'.'의 갯수가 3의 배수가 아니면 0출력 후 종료
 			cout << 0 << endl;
 		}
 		else {
-			solve(gameBoard, 0, 1, 1);
+			solve(gameBoard, 0);
 			cout << result << endl;
+			result = 0;
+			pointCnt = 0;
 		}
-
 	}
 
 	return 0;
 }
 
-void printBoard(char gameBoard[][21]) {
+void solve(char gameBoard[][25], int puzzleNum) {
+	int x, y = -1;
+	int breakFlag = 0;
+
 	for (int i = 1; i <= h; i++) {
 		for (int j = 1; j <= w; j++) {
-			cout << gameBoard[i][j] << " ";
+			if (gameBoard[i][j] == '.') {
+				x = i;
+				y = j;
+				breakFlag = 1;
+				break;
+			}
 		}
-		cout << endl;
+		if (breakFlag == 1)
+			break;
 	}
-	cout << endl;
-}
-
-void solve(char gameBoard[][21], int curPoint, int x, int y) {
-
-	//printBoard(gameBoard);
-
-	if (curPoint == pointCnt) {
+	if (puzzleNum == boardPuzzle)
 		result++;
-		return;
-	}
-	else if (x == w + 1) {
-		return;
-	}
 
-	int nextX, nextY;
-	if (y < w) {
-		nextX = x;
-		nextY = y + 1;
-	}
-	else if (y == w) {
-		nextX = x + 1;
-		nextY = 1;
-	}
+	if (y == -1)
+		return;
 
-	if (gameBoard[x][y] == '.') {
-		if (gameBoard[x][y + 1] == '.' && gameBoard[x + 1][y] == '.') {
-			gameBoard[x][y] = '#';
-			gameBoard[x][y + 1] = '#';
-			gameBoard[x + 1][y] = '#';
-			solve(gameBoard, curPoint + 3, nextX, nextY);
-			gameBoard[x][y] = '.';
-			gameBoard[x][y + 1] = '.';
-			gameBoard[x + 1][y] = '.';
-		}
-		if (gameBoard[x][y + 1] == '.' && gameBoard[x + 1][y + 1] == '.') {
-			gameBoard[x][y] = '#';
-			gameBoard[x][y + 1] = '#';
-			gameBoard[x + 1][y + 1] = '#';
-			solve(gameBoard, curPoint + 3, nextX, nextY);
-			gameBoard[x][y] = '.';
-			gameBoard[x][y + 1] = '.';
-			gameBoard[x + 1][y + 1] = '.';
-		}
-		if (gameBoard[x + 1][y] == '.' && gameBoard[x + 1][y + 1] == '.') {
-			gameBoard[x][y] = '#';
-			gameBoard[x + 1][y] = '#';
-			gameBoard[x + 1][y + 1] = '#';
-			solve(gameBoard, curPoint + 3, nextX, nextY);
-			gameBoard[x][y] = '.';
-			gameBoard[x + 1][y] = '.';
-			gameBoard[x + 1][y + 1] = '.';
-		}
-		if (gameBoard[x + 1][y] == '.' && gameBoard[x + 1][y - 1] == '.') {
-			gameBoard[x][y] = '#';
-			gameBoard[x + 1][y] = '#';
-			gameBoard[x + 1][y - 1] = '#';
-			solve(gameBoard, curPoint + 3, nextX, nextY);
-			gameBoard[x][y] = '.';
-			gameBoard[x + 1][y] = '.';
-			gameBoard[x + 1][y - 1] = '.';
-		}
+	int flag = 0;
+
+	if (gameBoard[x][y + 1] == '.' && gameBoard[x + 1][y] == '.') {
+		gameBoard[x][y] = '#';
+		gameBoard[x][y + 1] = '#';
+		gameBoard[x + 1][y] = '#';
+		solve(gameBoard, puzzleNum + 1);
+		gameBoard[x][y] = '.';
+		gameBoard[x][y + 1] = '.';
+		gameBoard[x + 1][y] = '.';
+		flag = 1;
 	}
-	solve(gameBoard, curPoint, nextX, nextY);
+	if (gameBoard[x][y + 1] == '.' && gameBoard[x + 1][y + 1] == '.') {
+		gameBoard[x][y] = '#';
+		gameBoard[x][y + 1] = '#';
+		gameBoard[x + 1][y + 1] = '#';
+		solve(gameBoard, puzzleNum + 1);
+		gameBoard[x][y] = '.';
+		gameBoard[x][y + 1] = '.';
+		gameBoard[x + 1][y + 1] = '.';
+		flag = 1;
+	}
+	if (gameBoard[x + 1][y] == '.' && gameBoard[x + 1][y + 1] == '.') {
+		gameBoard[x][y] = '#';
+		gameBoard[x + 1][y] = '#';
+		gameBoard[x + 1][y + 1] = '#';
+		solve(gameBoard, puzzleNum + 1);
+		gameBoard[x][y] = '.';
+		gameBoard[x + 1][y] = '.';
+		gameBoard[x + 1][y + 1] = '.';
+	}
+	if (gameBoard[x + 1][y] == '.' && gameBoard[x + 1][y - 1] == '.') {
+		gameBoard[x][y] = '#';
+		gameBoard[x + 1][y] = '#';
+		gameBoard[x + 1][y - 1] = '#';
+		solve(gameBoard, puzzleNum + 1);
+		gameBoard[x][y] = '.';
+		gameBoard[x + 1][y] = '.';
+		gameBoard[x + 1][y - 1] = '.';
+		flag = 1;
+	}
+	if (flag == 0)  //어느경우도 만족하지 않으면 현재 recursive 탐색 종료
+		return;
 }
